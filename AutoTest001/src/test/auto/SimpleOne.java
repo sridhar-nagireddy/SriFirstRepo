9e0701b380d64d044a5f6222c98d46fac8f22ab4
Sridhar <sridharnagireddy@gmail.com>
2018-04-09 17:02:12 +1000

    FirstPushFromEclipse

---
+++ AutoTest001/src/test/auto/SimpleOne.java
@@ -0,0 +1,10 @@
+package test.auto;
+
+public class SimpleOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("thi is the programoutput");
+	}
+
+}
